5674b084486d31e7a920fa28c2a38ddce6a2c9eb
Nursat Seitov <nursat.seitov12@gmail.com>
2024-07-23 18:18:59 +0500

    slug converter for product

--- goods/views.py
+++ goods/views.py
@@ -11,6 +11,11 @@ def catalog(request):
     return render(request,'goods/catalog.html',context)
 
 
-def product(request):
+def product(request,product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product':product
+    }
 
-    return render(request,'goods/product.html')
+
+    return render(request,'goods/product.html',context=context)
