dee0d944899ac542ad98a17e153ee4da58c4b31a
Nursat Seitov <nursat.seitov12@gmail.com>
2024-07-24 16:30:58 +0500

    the first pagination option

--- goods/urls.py
+++ goods/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'goods'
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
+
     path('product/<slug:product_slug>/', views.product, name='product'),
 
 ]
