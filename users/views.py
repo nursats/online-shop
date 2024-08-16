a5546cdb9d368eba45a7dc289b4235b752165b16
Nursat Seitov <nursat.seitov12@gmail.com>
2024-08-16 22:51:13 +0500

    registration form are controller are ready

--- users/views.py
+++ users/views.py
@@ -1,26 +1,57 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+from users.forms import UserLoginForm,UserRegistrationForm
+
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,password=password)
+            if user:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+    
+
     context = {
-        'tilte':'Home - Авторизация',
+        'title':'Home - Авторизация',
+        'form':form,
     }
     
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+                form.save()
+                user = form.instance
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+    
+
     context = {
-        'tilte':'Home - Регистрация',
+        'title':'Home - Регистрация',
+        'form':form, 
     }
 
     return render(request, 'users/registration.html', context)
 
 def profile(request):
     context = {
-        'tilte':'Home - Кабинет',
+        'title':'Home - Кабинет',
     }
 
     return render(request, 'users/profile.html', context)
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
