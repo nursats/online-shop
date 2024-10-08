a28af90212d95c759772a60f986f1b689964c82c
Nursat Seitov <nursat.seitov12@gmail.com>
2024-10-08 16:24:38 +0500

    working with cart functionality added

--- carts/urls.py
+++ carts/urls.py
@@ -6,5 +6,6 @@ app_name = 'carts'
 urlpatterns = [
     path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
     path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),
+    path('cart_dicrement/<slug:product_slug>/', views.cart_dicrement, name='cart_dicrement'),
     path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import Cart
 from goods.models import Products
 
@@ -18,14 +18,23 @@ def cart_add(request,product_slug):
     return redirect(request.META['HTTP_REFERER'])
 
 def cart_change(request, product_slug):
-    ...
+    product = Products.objects.get(slug=product_slug)
 
+    cart = get_object_or_404(Cart, user=request.user, product=product)
+    cart.quantity += 1
+    cart.save()
+    return redirect(request.META['HTTP_REFERER'])
 
+def cart_dicrement(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    cart = get_object_or_404(Cart, user=request.user, product=product)
+    cart.quantity -= 1 
+    if cart.quantity < 1:
+        cart.delete()
+        return redirect(request.META['HTTP_REFERER'])
+    cart.save()
+    return redirect(request.META['HTTP_REFERER'])
     
-
-
-
-
 
 def cart_remove (request, cart_id):
     
